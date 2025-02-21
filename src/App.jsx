1d167cf33ab22acbec1560c3c1abd92ee435cec6
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2025-02-21 18:55:49 +0300

    [FEAT-10] Add page elements to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import About from "./components/About";
+import Speakers from "./components/Speakers";
+import Skydaysctf from "./components/Skydaysctf";
+import Questions from "./components/Questions";
+import Sponsors from "./components/Sponsors";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Welcome />
+        <About />
+        <Speakers />
+        <Skydaysctf />
+        <Questions />
+        <Sponsors />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
