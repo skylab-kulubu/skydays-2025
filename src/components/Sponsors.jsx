13cf419c2f739b0d1ed9ad3ebe38ddf352f60628
Ahmet Rauf OKTAY <68822176+ahmetraufoktay@users.noreply.github.com>
2025-02-21 18:53:50 +0300

    [FEAT-7] Add sponsors page with style

---
+++ src/components/Sponsors.jsx
@@ -0,0 +1,25 @@
+import "./Sponsors.css";
+
+function Sponsors() {
+  return (
+    <article className="sponsors">
+      <h2>Sponsorlarımız</h2>
+      <div className="sponsor-container">
+        <div>
+          <h3>Ana Sponsor</h3>
+          <div></div>
+        </div>
+        <div>
+          <h3>Gümüş Sponsor</h3>
+          <div></div>
+        </div>
+        <div>
+          <h3>Bronz Sponsor</h3>
+          <div></div>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default Sponsors;
